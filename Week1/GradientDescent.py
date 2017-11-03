eacc82127ff8200df1d8a635a88f4fb003fa3806
HarshavardhanK <harshavardhan816@gmail.com>
2017-11-03 18:42:20 +0530

    Fourth Commit

--- Week1/GradientDescent.py
+++ Week1/GradientDescent.py
@@ -1,46 +1,76 @@
 # Gradient Descent implemented in Python with numpy module
 import numpy as ny
+import matplotlib.pyplot as plt
 
 def SquaredCostFunction(X, t, Y, size):
 	f = ny.dot(X, t) # ignoring theta0 (yet)
 	l = f - Y
-	cost = ny.sum(l ** 2) / (2 * size) # / for the cost to be independant of the training set size 
+	cost = ny.sum(l ** 2) / (2 * size) # / for the cost to be independant of the training set size
 
 	return cost
 
 print(SquaredCostFunction(ny.matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), 4, ny.matrix([[8, 0, 0], [4, 9, 2], [6, 7, 8]]), 3))
 
-def GradientDescent(X, Y, c, m, a, size, times):
+def GradientDescent(X, Y, a, times):
+
 	X_trans = X.transpose()
+	size = X.shape[0]
+	m = ny.ones(2)
 
 	for i in range(0, times):
 
-		f = c + ny.dot(X, m) # taking f(X) := c + mX
+		f = ny.dot(X, m) # taking f(X) := c + mX
 		loss = f - Y
-		print(loss)
+		#print(loss)
 		cost = ny.sum(loss ** 2) / (2 * size)
 
 		# take partial derivative manually..
 
-		g0 = loss / size 
+	#	g0 = loss / size
 		g1 = ny.dot(X_trans, loss) / size
 
-		#update the theta values
-
-		c = c - a * g0
+		#update the m values
 		m = m - a * g1
 
-		print(str(i) + " round, cost is " + str(cost))
+		print(str(i + 1) + " iter, cost is " + str(cost))
 
-	return (c, m)
-
-
-X = ny.matrix([[1], [4], [6]])
-#print(X)
-Y = ny.matrix([[6, 6, 7], [8, 6, 7], [3, 4 ,9]])
-#print(Y)
-
-t = GradientDescent(X, Y, 2, 3, 3.14, 3, 50)
-print(t)
+	return m
 
 
+'''X = ny.matrix([[1], [4], [6]])
+print(ny.shape(X))
+Y = [6, 6, 7]
+print(ny.shape(Y))
+
+(c, m) = GradientDescent(X, Y, 2, 3, 3.14, 3, 50) # deprecated function
+
+H = c + m * X
+print(H)'''
+
+path="/Users/HarshavardhanK/Downloads/machine-learning-ex1/ex1/ex1data1.txt"
+data_file=open(path, 'r')
+x_arr = []
+y_arr = []
+
+with open(path, "r") as f:
+    for l in f:
+
+        currentL = l.split(",")
+        x_arr.append(float(currentL[0]))
+        y_arr.append(float(currentL[1]))
+
+'''for i in range(0, 3):
+    x_arr.append(0)
+    y_arr.append(0)'''
+
+X = ny.array(x_arr)
+X = X.reshape(97, 1)
+y_arr = ny.array(y_arr)
+#y_arr = y_arr.reshape(20,5)
+(m, n) = ny.shape(X)
+print((m, n))
+
+X = ny.c_[ ny.ones(m), X] # insert column
+a = 0.02 # learning rate
+theta = GradientDescent(X, y_arr,a, 10000)
+print(theta)
